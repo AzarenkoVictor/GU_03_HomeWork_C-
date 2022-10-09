a767fa388178af7c8011dde5db13b1a3f54f3eb2
Victor Azarenko <victor.azarenko@gmail.com>
2022-10-09 09:56:42 +0300

    Добавлено решение 19 Задачи

--- 19/Program.cs
+++ 19/Program.cs
@@ -3,3 +3,14 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if ((a / 10000 == a % 10) || (a / 1000 == a % 100))
+{
+    Console.WriteLine("Введенно число " + a + " является полиндромом");
+}
+else
+{
+    Console.WriteLine("Введенно число " + a + " не является полиндромом");
+}

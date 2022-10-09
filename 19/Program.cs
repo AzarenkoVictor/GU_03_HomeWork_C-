f98f0d41a5a775fe60573c40819dfd289a28530a
Victor Azarenko <victor.azarenko@gmail.com>
2022-10-09 21:57:17 +0300

    Добавил 2е решение 19й Задачи с возможностью вводв числа любой длинны

--- 19/Program.cs
+++ 19/Program.cs
@@ -4,13 +4,37 @@
 // 12821 -> да
 // 23432 -> да
 
+// // Решение 1 - Математика
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if ((a / 10000 == a % 10) && ((a-(a/10000)*10000)) / 1000 == (((a % 100) / 10)))
+// {
+//     Console.WriteLine("Введенное число " + a + " является полиндромом");
+// }
+// else
+// {
+//     Console.WriteLine("Введенное число " + a + " не является полиндромом");
+// }
+
+// Решение 2 - Строки
+
 Console.WriteLine("Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
-if ((a / 10000 == a % 10) && ((a-(a/10000)*10000)) / 1000 == (((a % 100) / 10)))
+string n = Convert.ToString(a);
+int length = n.Length-1;
+int z = 0;
+for (int i = 0; i <= (length / 2); i++)
 {
-    Console.WriteLine("Введенное число " + a + " является полиндромом");
+    if (n[i] != n[length - i])
+    {
+        z = z + 1;
+    }
 }
-else
+if (z == 0)
 {
-    Console.WriteLine("Введенное число " + a + " не является полиндромом");
+    Console.WriteLine("Число является полиндромом");
+}
+if (z > 0)
+{
+    Console.WriteLine("Число не является полиндроном");
 }

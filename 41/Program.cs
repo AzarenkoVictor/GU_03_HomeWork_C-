78c3a2e75642c7713f3ff9cddee79e2769dd1575
Victor Azarenko <victor.azarenko@gmail.com>
2022-10-20 22:01:34 +0300

    Добавлена 41 Задача

---
+++ 41/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("Введите число - подсчитаем количество положительных. Для выхода нажмите 0 "); int number = Convert.ToInt32(Console.ReadLine());
+int amount = 0;
+if (number > 0) { amount = 1; }
+while (number != 0)
+{
+    Console.WriteLine($"Введите число    Введено {amount} положительных чисел"); number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0) { amount = amount + 1; }
+}
+Console.WriteLine($"Итого: введено {amount} положительных чисел");
